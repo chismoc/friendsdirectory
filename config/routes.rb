20d3be8ee3e2578bec1693a430b1bea390d9786b
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-10-31 12:13:49 +0200

    add header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+ # change this with 'home#index' to mahe index page a root page get 'home/index'
+  root 'home#index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
