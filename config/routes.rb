7768758a7bc22dbfe1e9fc10fc40faade43fe4e7
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-11-02 19:54:47 +0200

    polish up views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
  # change this with 'home#index' to mahe index page a root page get 'home/index'
-  root 'home#index'
+  root 'friends#index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
